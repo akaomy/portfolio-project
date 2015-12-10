1496d7245a8e9f7c15dcda1541b66e2af9ec258a
akaomy <annakozlova92@gmail.com>
2015-12-09 17:38:34 -0800

    bug fix: fix layout, make it more '/pixel perfect'/

--- app/js/main.js
+++ app/js/main.js
@@ -1,11 +1,5 @@
 $(document).ready(function() {
 	
-	console.log("i am ready");
-
-	// $('.navigation-link').on('click', function(e) {
-	// 	e.preventDefault();
-
-	// 	$(".navigation-link").css("color", "red");
-	// });)
+	console.log("working");
 
 });
